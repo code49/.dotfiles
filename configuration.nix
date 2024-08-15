356bb789161e881fbbd94490f1c1c840d689abf1
code49 <davidlechan@gmail.com>
2024-08-14 17:47:21 -0700

    updated config files to revert to using nix-approved hyprland version (instead of the latest git release)

--- configuration.nix
+++ configuration.nix
@@ -64,18 +64,9 @@
     pulse.enable = true;
   };
 
-  # caching for hyprland git version 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys =
-      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   # Enabling hyprland
   programs.hyprland = {
     enable = true;
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; # enable git version of hyprland
     xwayland.enable = true;
   };
 
--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -4,8 +4,6 @@
   wayland.windowManager.hyprland = {
     enable = true;
 
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
 
     settings = {
