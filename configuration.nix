ec4023be75ba55a22b61d9bd8ac914ca1e2fc618
Theo Rode <theorodester@gmail.com>
2025-02-09 15:27:59 -0800

    I probably broke something? I can't update :(

--- configuration.nix
+++ configuration.nix
@@ -65,17 +65,17 @@
   };
 
   # caching for hyprland git version 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys =
-      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
+  # nix.settings = {
+  #   substituters = [ "https://hyprland.cachix.org" ];
+  #   trusted-public-keys =
+  #     [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  # };
 
   # Enabling hyprland
   programs.hyprland = {
     enable = true;
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; # enable git version of hyprland
+    # package =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland; # enable git version of hyprland
     xwayland.enable = true;
   };
 
--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland = {
     enable = true;
 
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # package =
+    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     xwayland.enable = true;
 
     extraConfig = ''
