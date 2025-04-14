ab5141caae662ececd29586276578980ed740d38
Theo Rode <theorodester@gmail.com>
2025-04-14 11:52:08 -0700

    this fixes it?

--- configuration.nix
+++ configuration.nix
@@ -259,10 +259,12 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gtk2";
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
