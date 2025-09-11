8eafc8bb3fe6147346ab02a545a6ac71d3c62c5c
David Chan <davidlechan@gmail.com>
2025-09-11 17:32:24 -0400

    added print statement to gitac alias

--- home/home.nix
+++ home/home.nix
@@ -138,7 +138,7 @@
     shellAliases = {
       ls = "ls -a";
       ".." = "cd ..";
-      "gitac" = "git status; git add -A; git commit -m";
+      "gitac" = "git status; echo 'adding all changes + committing them for:' ; pwd;  git add -A; git commit -m";
       "ssh" = "kitten ssh";
       "b" = "btop";
       "n" = "nvtop";
